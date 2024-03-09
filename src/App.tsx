e6e9fd8fb368df0e5d4424aea88ecc28af5457ae
patrickpetty23 <pettyp@icloud.com>
2024-03-08 18:09:09 -0700

    finished up Mission 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './.CollegeBasketballTeams.json';
+
+const teams = data.teams;
+
+type TeamProps = (typeof data.teams)[0];
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city},{oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>March Madness 2024</h1>
+    </div>
+  );
+}
+
+// Function to render team names
+function TeamNames() {
+  return (
+    <div>
+      {teams.map((oneTeam) => (
+        <Team {...oneTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* Call the renderTeamNames function */}
+      <TeamNames />
     </div>
   );
 }
